cb73c245793a0fee20a5841cbe381e06c5949e2a
Darrel <darrel124568@gmail.com>
2026-07-23 09:39:48 +0300

    final commit

--- server/models.py
+++ server/models.py
@@ -8,45 +8,47 @@ db = SQLAlchemy(metadata=metadata)
 class Driver(db.Model):
     __tablename__ = 'drivers'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    truck = db.relationship('Truck', back_populates='driver', uselist=False)
+    name = db.Column(db.String, nullable=False)
+    truck = db.relationship('Truck', back_populates='driver', uselist=False, cascade='all, delete-orphan')
+    trips = db.relationship('Trip', back_populates='driver', cascade='all, delete-orphan')
 
 class Truck(db.Model):
     __tablename__ = 'trucks'
     id = db.Column(db.Integer, primary_key=True)
-    plate_number = db.Column(db.String)
-    driver_id=db.Column(db.Integer, db.ForeignKey('drivers.id'))
-    expenses = db.relationship('Expense', back_populates='truck', uselist=True)
+    plate_number = db.Column(db.String, nullable=False, unique=True)
+    driver_id = db.Column(db.Integer, db.ForeignKey('drivers.id'), unique=True)
+    expenses = db.relationship('Expense', back_populates='truck', cascade='all, delete-orphan')
     driver = db.relationship('Driver', back_populates='truck', uselist=False)
-    trips = db.relationship('Trip', back_populates='truck')
+    trips = db.relationship('Trip', back_populates='truck', cascade='all, delete-orphan')
 
 
 class Expense(db.Model):
     __tablename__ = 'expenses'
     id = db.Column(db.Integer, primary_key=True)
-    description=db.Column(db.String)
-    expense_date = db.Column(db.String)
-    truck_id= db.Column(db.Integer, db.ForeignKey('trucks.id'))
+    description = db.Column(db.String, nullable=False)
+    expense_date = db.Column(db.String, nullable=False)
+    truck_id = db.Column(db.Integer, db.ForeignKey('trucks.id'), nullable=False)
     truck = db.relationship('Truck', back_populates='expenses', uselist=False)
 
 class Trip(db.Model):
-    __tablename__= 'trips'
-
-    id = db.Column(db.Integer, primary_key = True)
-    trip_date = db.Column(db.String)
-    origin = db.Column(db.String)
-    distance = db.Column(db.String)
-    destination = db.Column(db.String)
-    driver_id = db.Column(db.Integer, db.ForeignKey('drivers.id')) 
-    truck_id = db.Column(db.Integer, db.ForeignKey('trucks.id'))
-    truck = db.relationship('Truck', back_populates='trips')
-    revenue = db.relationship('Revenue', back_populates='trips')
-
-class Revenue(db.Model):
-    __tablename__='revenues'
+    __tablename__ = 'trips'
 
     id = db.Column(db.Integer, primary_key=True)
-    amount = db.Column(db.Float)
-    trip_id = db.Column(db.Integer, db.ForeignKey('trips.id')) 
+    trip_date = db.Column(db.String, nullable=False)
+    origin = db.Column(db.String, nullable=False)
+    distance = db.Column(db.String, nullable=False)
+    destination = db.Column(db.String, nullable=False)
+    driver_id = db.Column(db.Integer, db.ForeignKey('drivers.id'), nullable=False)
+    truck_id = db.Column(db.Integer, db.ForeignKey('trucks.id'), nullable=False)
+    driver = db.relationship('Driver', back_populates='trips')
+    truck = db.relationship('Truck', back_populates='trips')
+    revenue = db.relationship('Revenue', back_populates='trip', uselist=False, cascade='all, delete-orphan')
 
-    trips = db.relationship('Trip', back_populates='revenue')
+class Revenue(db.Model):
+    __tablename__ = 'revenues'
+
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    trip_id = db.Column(db.Integer, db.ForeignKey('trips.id'), nullable=False, unique=True)
+
+    trip = db.relationship('Trip', back_populates='revenue')
